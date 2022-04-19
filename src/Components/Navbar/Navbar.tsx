943959eefe9e44ae7e8987c6fc71099e66f1febd
Weichen Tie <75655299+WeichenTie@users.noreply.github.com>
2022-04-19 23:36:51 +1000

    added basic ui

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,19 +1,14 @@
-import { useState } from 'react';
+import { useState, useContext, useEffect, useLayoutEffect } from 'react';
 import './Navbar.css'
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 
 const NavItem = (props: any) => {
-
     const [isHovered, setIsHovered] = useState(false);
-
 
     return (
         <motion.li className='nav-item'
             onMouseEnter={() => setIsHovered(true)}
             onMouseLeave={() => setIsHovered(false)}
-
-            initial={{opacity:0}}
-            animate={{opacity:1}}
         >
             {props.children}
             {isHovered && <NavUnderline></NavUnderline>}
@@ -22,23 +17,30 @@ const NavItem = (props: any) => {
 }
 const NavUnderline = () => {
     return (
-        <motion.div layoutId='nav-underline' className='nav-underline'>
-        </motion.div>
+        <AnimatePresence>
+            <motion.div
+                layoutId='nav-underline'
+                className='nav-underline'
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}>
+            </motion.div>
+        </AnimatePresence>
     )
 }
 
 
 const Navbar = () => {
-
-
+    window.addEventListener('scroll', (e) =>{console.log(e);
+    });
     return (
-        <ul id="navbar">
+        <motion.ul id="navbar">
             <NavItem>Home</NavItem>
             <NavItem>About</NavItem>
             <NavItem>Blogs</NavItem>
             <NavItem>Projects</NavItem>
             <NavItem>Contact Me</NavItem>
-        </ul>
+        </motion.ul>
     )
 }
 
